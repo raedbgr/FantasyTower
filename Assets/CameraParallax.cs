c286884fd88a51deffac5f9bcc9ca26112d3f3c8
Raed <raed.bouguerra@outlook.com>
2021-06-29 16:12:00 +0100

    first commit

---
+++ Assets/CameraParallax.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraParallax : MonoBehaviour
+{
+   // public GameObject envirenment;
+    public Camera mainCam;
+    public float upYpos;
+
+   
+    // Update is called once per frame
+    void Start()
+    {
+        mainCam = Camera.main;
+    }
+    private void OnBecameInvisible()
+    {
+        Vector2 upPos = new Vector2(transform.position.x, mainCam.transform.position.y + upYpos);
+        transform.position = upPos;
+            }
+}
